4b4b154637ce12f9a3a332694621d5cee7e7d841
Hyka74 <67745020+Hyka74@users.noreply.github.com>
2026-07-16 14:41:35 +0200

    Update Calculateur_coef.py

--- Calculateur_coef.py
+++ Calculateur_coef.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Calculateur Coef & Écotaxe", page_icon="📝", l
 st.title("📝 Calcul Tarif + écotaxe")
 
 # 1. Zones de saisie
-prix_achat = st.number_input("Prix d'achat HT/TTC (€)", format="%.2f")
+prix_achat = st.number_input("Prix d'achat HT/TTC (€)" format="%.2f")
 coefficient = st.number_input("Coeff", format="%.2f")
 ecotaxe = st.number_input("Écotaxe HT (€)", format="%.2f")
 
